517ebce340843d8eece1982638b55fe12d196f1e
Dhruvi2603 <pateldhruvi2611@gmail.com>
2024-09-08 12:51:08 +0530

    Update ProfilePage.jsx

--- frontend/src/scenes/profilePage/ProfilePage.jsx
+++ frontend/src/scenes/profilePage/ProfilePage.jsx
@@ -16,7 +16,7 @@ const ProfilePage = () => {
  const mode = useSelector((state) => state.mode);
  
  const getUser = async () => {
-  const response = await fetch(`http://localhost:3001/users/${userId}`, {
+  const response = await fetch(`https://connectwave-backend.onrender.com/users/${userId}`, {
     method: "GET",
     headers: { Authorization: `Bearer ${token}` }
   })
