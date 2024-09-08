22df81ccc60bab292b1f837381462d9477a7bca1
Dhruvi2603 <pateldhruvi2611@gmail.com>
2024-09-08 12:50:26 +0530

    Update FriendListWidget.jsx

--- frontend/src/scenes/widgets/FriendListWidget.jsx
+++ frontend/src/scenes/widgets/FriendListWidget.jsx
@@ -12,7 +12,7 @@ const FriendListWidget = ({ userId }) => {
   const getFriends = async () => {
     try {
       const response = await fetch(
-        `http://localhost:3001/users/${userId}/friends`,
+        `https://connectwave-backend.onrender.com/users/${userId}/friends`,
         {
           headers: {
             Authorization: `Bearer ${token}`,
