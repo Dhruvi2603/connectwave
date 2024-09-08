641abb2651d9481c2ca82fde3d43fcbe2b98fc3f
Dhruvi2603 <pateldhruvi2611@gmail.com>
2024-09-08 13:19:24 +0530

    Update Form.jsx

--- frontend/src/scenes/loginPage/Form.jsx
+++ frontend/src/scenes/loginPage/Form.jsx
@@ -59,7 +59,7 @@ const Form = () => {
     formData.append("picturePath", values.picture.name);
 
     const savedUserResponse = await fetch(
-      "http://localhost:3001/auth/register",
+      "https://connectwave-backend.onrender.com/auth/register",
       {
         method: "POST",
         body: formData,
