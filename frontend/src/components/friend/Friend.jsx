551d5fd7060daa3192a45e58e6a48d76057fe436
Dhruvi2603 <pateldhruvi2611@gmail.com>
2024-09-08 12:53:40 +0530

    Update Friend.jsx

--- frontend/src/components/friend/Friend.jsx
+++ frontend/src/components/friend/Friend.jsx
@@ -19,7 +19,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 
 
   const patchFriend = async () => {
-    const response = await fetch(`http://localhost:3001/users/${_id}/${friendId}`, {
+    const response = await fetch(`https://connectwave-backend.onrender.com/users/${_id}/${friendId}`, {
       method: "PATCH",
       headers: {
         Authorization: `Bearer ${token}`,
