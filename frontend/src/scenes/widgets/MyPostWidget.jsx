07aa924e3ba98b25e94b6499fae296b58db42e3d
Dhruvi2603 <pateldhruvi2611@gmail.com>
2025-08-03 14:51:38 +0530

    Update MyPostWidget.jsx

--- frontend/src/scenes/widgets/MyPostWidget.jsx
+++ frontend/src/scenes/widgets/MyPostWidget.jsx
@@ -28,7 +28,7 @@ const MyPostWidget = ({ picturePath }) => {
     console.log("FormData content:", formData);
   
     try {
-      const response = await fetch(`http://connectwave-backend.onrender.com/posts`, {
+      const response = await fetch(`https://connectwave-backend.onrender.com/posts`, {
         method: "POST",
         headers: { Authorization: `Bearer ${token}` },
         body: formData,
