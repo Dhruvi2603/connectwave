ee76795b5e4ebb51cc5f54e98585877ad3b72b86
Dhruvi2603 <pateldhruvi2611@gmail.com>
2025-08-03 14:00:20 +0530

    Update FriendsChatWidget.jsx

--- frontend/src/scenes/widgets/FriendsChatWidget.jsx
+++ frontend/src/scenes/widgets/FriendsChatWidget.jsx
@@ -15,7 +15,7 @@ const FriendsChatWidget = ({ userId, onSelectFriend }) => {
   const getFriends = async () => {
     try {
       const response = await fetch(
-        `http://localhost:3001/users/${userId}/friends`,
+        `http://connectwave-backend.onrender.com/users/${userId}/friends`,
         {
           headers: {
             Authorization: `Bearer ${token}`,
