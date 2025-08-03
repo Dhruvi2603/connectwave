d32bc31b8f4bac8772aa7eef847e95839a468a45
Dhruvi2603 <pateldhruvi2611@gmail.com>
2025-08-03 20:26:51 +0530

    Update UserImage.jsx

--- frontend/src/components/useImage/UserImage.jsx
+++ frontend/src/components/useImage/UserImage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const UserImage = ({ image }) => {
   return (
     <div className='w-[60px] h-[60px]'>
-      <img src={`http://localhost:3001/assets/${image}`} alt="user" className=' object-cover rounded-[50%] w-[60px] h-[60px]' />
+      <img src={`https://connectwave-backend.onrender.com/assets/${image}`} alt="user" className=' object-cover rounded-[50%] w-[60px] h-[60px]' />
     </div>
   )
 }
