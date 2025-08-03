7017d252dc1f1cba37c2917f1ed6cdfc45aeddd5
Dhruvi2603 <pateldhruvi2611@gmail.com>
2025-08-03 14:52:32 +0530

    Update UserWidget.jsx

--- frontend/src/scenes/widgets/UserWidget.jsx
+++ frontend/src/scenes/widgets/UserWidget.jsx
@@ -14,7 +14,7 @@ const UserWidget = ({ userId, picturePath }) => {
     
    
     const getUser = async () => {
-        const response = await fetch(`http://connectwave-backend.onrender.com/users/${userId}`,
+        const response = await fetch(`https://connectwave-backend.onrender.com/users/${userId}`,
             {
                 method: "GET",
                 headers: { Authorization: `Bearer ${token}` },
