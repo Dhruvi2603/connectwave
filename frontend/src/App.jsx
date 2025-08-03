c6b304e5a1cf481189cba6b910fc4dd67af6dc60
Dhruvi2603 <pateldhruvi2611@gmail.com>
2025-08-03 14:53:05 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
   useEffect(() => {
     if (user && !socketRef.current) {
-      const socketConnection = io("http://connectwave-backend.onrender.com", {
+      const socketConnection = io("https://connectwave-backend.onrender.com", {
         query: {
           userId: user._id,
         },
